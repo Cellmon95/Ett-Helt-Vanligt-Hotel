4c3e26f1a5949cc63ae19de639c29420aa557bee
Lucas Källberg <lucas.arne@hotmail.com>
2023-01-11 20:48:39 +0100

    tried fixing booking

--- book.php
+++ book.php
@@ -3,4 +3,23 @@
 
 declare(strict_types=1);
 
-var_dump(file_get_contents('https://www.yrgopelago.se/centralbank/islands'));
+$postdata = json_encode([
+    "form_params" => [
+        "transferCode" => '2e3c06ea-3b95-4ca6-b2ba-4346d9005d9e',
+        "totalCost" => 10
+    ]
+]);
+
+$options = [
+    'http' => [
+        'method' => 'POST',
+        'header' => 'Content-Type: application/json;',
+        'content' => $postdata
+    ]
+];
+
+$context  = stream_context_create($options);
+
+$result = file_get_contents('https://www.yrgopelago.se/centralbank/transfercode', false, $context);
+header('Content-Type: application/json');
+echo $result;
